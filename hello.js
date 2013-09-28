3a92d0aae6f10e91377af9ae19ccc32ea65eff44
Lukas Ender <hello@lukasender.at>
2013-09-29 00:32:35 +0200

    removed semi-colon to fail build process.

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World :)\n');
+    res.end('Hello World :)\n') // removed semi-colon to fail build process.
 }).listen(1338, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1338/');
